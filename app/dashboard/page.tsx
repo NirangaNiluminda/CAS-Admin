bfb282968844563b909f075e6cd20970802042e9
Kavindu Dhananjaya <rathnayaka_rmkd_e23@engug.ruh.ac.lk>
2024-10-09 13:20:19 +0530

    minor changes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -63,7 +63,7 @@ const Page = () => {
     };
 
     const getQuizLink = (id: string) => {
-        const URL = `http://localhost:3002/${id}`;
+        const URL = `http://localhost:3002/assignment/${id}`;
         console.log(URL);
 
         // Copy the URL to the clipboard
